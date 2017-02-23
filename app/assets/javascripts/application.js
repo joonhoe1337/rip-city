b147be4604acbd23bb6e573adc59b7784c6aaf40
abcSup <kristan97@hotmail.com>
2017-02-23 17:36:29 +0800

    Edit weird bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,16 +18,3 @@
 //= require bootstrap-sprockets
 //
 
-$(function() {
-  $("#datepicker-checkin").datepicker({
-    numberOfMonths: [1, 2],
-    dateFormat: "dd-mm-yy",
-    altField: "#altfield-checkin"
-  });
-
-  $("#datepicker-checkout").datepicker({
-    numberOfMonths: [1, 2],
-    dateFormat: "dd-mm-yy",
-    altField: "#altfield-checkout"
-  });
-});
--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -1,12 +1,14 @@
 $(function() {
   $("#datepicker-checkin").datepicker({
     numberOfMonths: [1, 2],
+    minDate: +1,
     dateFormat: "dd-mm-yy",
     altField: "#altfield-checkin"
   });
 
   $("#datepicker-checkout").datepicker({
     numberOfMonths: [1, 2],
+    minDate: +1,
     dateFormat: "dd-mm-yy",
     altField: "#altfield-checkout"
   });
